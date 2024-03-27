ae671b595ff43351f9adca7ac5ea961964c27546
Srikanth K <kolleparasrikanth@gmail.com>
2024-03-27 08:53:08 +0530

    loading previous messages, broadcasting Implemented

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import http from "http";
 
 
 import { connect } from './config.js';
+import { chatModel } from './chart.schema.js';
 
 
 const app = express();
@@ -20,8 +21,37 @@ const io = new Server(server, {
 });
 
 
-io.on("connection", function () {
+io.on("connection", function (socket) {
     console.log("connection is Established");
+
+    socket.on("join", (data) => {
+        socket.username = data;
+        // send old messages to the clients.
+        chatModel.find().sort({ timestamp: 1 }).limit(50)
+            .then(messages => {
+                socket.emit('load_messages', messages);
+            }).catch(err => {
+                console.log(err);
+            })
+    });
+
+    socket.on('new_message', (data) => {
+        let userMessage = {
+            username : socket.username,
+            message : data
+        }
+
+        const newMessage = new chatModel({
+            username : socket.username,
+            message : data,
+            timestamp : new Date()
+        })
+
+        newMessage.save();
+
+        socket.broadcast.emit('broadcast_message', userMessage);
+    })
+
 })
 
 server.listen(3200, () => {
